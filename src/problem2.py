50986f27a7a5e899c9d89fc992264f014461981e
haoy1 <haoy1@rose-hulman.edu>
2018-09-16 17:49:15 -0400

    .

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Valerie Galluzzi, Mark Hays,
-         Amanda Stouder, their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Amanda Stouder, their colleagues and Brandon Hao.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -101,6 +101,7 @@ def problem2a(circle, rectangle, window):
       :type rectangle: rg.Rectangle
       :type window:    rg.RoseWindow
     """
+
     # ------------------------------------------------------------------
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
@@ -110,6 +111,16 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.continue_on_mouse_click()
+    line = rg.Line(rectangle.get_upper_right_corner(), rectangle.get_lower_left_corner())
+    line.attach_to(window)
+    window.continue_on_mouse_click()
+    circle.fill_color = rectangle.outline_color
+
+    window.render()
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -181,7 +192,12 @@ def problem2b(rect, n, delta, win):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 to 25 minutes.
     # ------------------------------------------------------------------
+    rect.attach_to(win)
+    for k in range(n):
+        rectangle = rg.Rectangle(rg.Point(rect.get_upper_left_corner().x-delta*(k+1),rect.get_upper_left_corner().y-delta*(k+1)),rg.Point(rect.get_lower_right_corner().x+delta*(k+1),rect.get_lower_right_corner().y+delta*(k+1)))
+        rectangle.attach_to(win)
 
+    win.render()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
